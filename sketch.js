d1771fa2db89534a8408827c22bd26ae0c4e6af6
Slaton <slsp8752@colorado.edu>
2015-10-26 22:05:06 -0600

    Added a second sprite/image

--- sketch.js
+++ sketch.js
@@ -16,7 +16,10 @@ var y_velocity = 0;
 var acceleration = 10;
 var image_width = 0;
 var image_height = 0;
+var image2_width = 0;
+var image2_height = 0;
 var img;
+var img2;
 var start = 0;
 var urls = ["https://pbs.twimg.com/profile_images/429055564262277120/fjuVYyFu.jpeg","https://pbs.twimg.com/profile_images/551143684671291392/Nx_lx21L_400x400.jpeg","http://chan.catiewayne.com/b/src/137230208038.jpg"];
 
@@ -24,9 +27,11 @@ function setup() {
   var canv = createCanvas(windowWidth,windowHeight);
   canv.parent("bgCanvas");
   s = createSprite(0,0,0,0);
+  s2 = createSprite(0,0,0,0);
   line = createSprite(width/2,300,width,2);
   line.immovable = true;
   img = createImg("blank.png");
+  img2 = createImg("blank.png");
 }
 
 function draw() {
@@ -42,12 +47,15 @@ function draw() {
   if(start == 1){
     //the image will follow the image's movement, so we can use collision from p5sprite on the DOM
     img.position(s.position.x-(img_width/2), s.position.y-(img_height/2));
+    img2.position(s2.position.x-(img_width/2), s2.position.y-(img_height/2));
     zz = img.position();
     //floored since sprites can have non integer positions, while DOM image objects can't
     y_velocity += Math.floor(acceleration);
     s.position.y = s.position.y + y_velocity;
+    s2.position.y = s2.position.y + y_velocity;
     //check collision with line
     s.bounce(line, invert);
+    s2.bounce(line, invert);
   }
   drawSprites();
 }
@@ -55,10 +63,11 @@ function draw() {
 function invert(){
     y_velocity = Math.floor(y_velocity * -0.5);
     s.position.y = 171;
+    s2.position.y = 171;
 }
 
 function checkDimensions(){
-    if (img_width != 0 && img_height != 0) {
+    if (img_width != 0 && img_height != 0 && img2_width != 0 && img2_height != 0) {
         spawn();
     }
     else {
@@ -70,9 +79,11 @@ function checkDimensions(){
 
 function spawn(){
     //makes our DOM image object un-highlight-able, so it's indistinguishable from the canvas
-    img.class("noselect")
-    s = createSprite(width/2, 10, img_width, img_height);
+    img.class("noselect");
+    s = createSprite(width/3, 10, img_width, img_height);
+    s2 = createSprite(2*(width/3), 10, img_width, img_height);
     s.shapeColor = color(0, 0, 0, 0);
+    s2.shapeColor = color(0, 0, 0, 0);
     img.position(s.positon.x-(img_width/2), 10-(img_height/2));
 }
 
@@ -81,10 +92,15 @@ function mousePressed() {
     y_velocity = 0;
     acceleration = 1;
     img.remove();
+    img2.remove();
     s.remove();
+    s2.remove();
     img = createImg(urls[ Math.floor( Math.random() * urls.length ) ]);
+    img2 = createImg(urls[ Math.floor( Math.random() * urls.length ) ]);
     img_width = img.width;
     img_height = img.height;
+    img2_width = img2.width;
+    img2_height = img2.height;
     //Make sure img_width and image_height have values before drawing them, so the image collides properly.
     checkDimensions();
 }
