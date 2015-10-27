81fc9052df64cfc1c96c0d895de81363da435002
Slaton <slsp8752@colorado.edu>
2015-10-26 22:50:44 -0600

    different sized images now bounce correctly

--- sketch.js
+++ sketch.js
@@ -13,17 +13,18 @@ To deal with this, we subtract half the image width/height from the values of th
 */
 
 var y_velocity = 0;
+var y2_velocity = 0;
 var acceleration = 10;
-var image_width = 0;
-var image_height = 0;
-var image2_width = 0;
-var image2_height = 0;
+var img_width = 0;
+var img_height = 0;
+var img2_width = 0;
+var img2_height = 0;
 var line_height = 400;
 var img;
 var img2;
 var start = false;
 var stopped = false;
-var urls = ["https://pbs.twimg.com/profile_images/429055564262277120/fjuVYyFu.jpeg","https://pbs.twimg.com/profile_images/551143684671291392/Nx_lx21L_400x400.jpeg","http://chan.catiewayne.com/b/src/137230208038.jpg"];
+var urls = ["http://petcaretips.net/turtle_waving.gif", "https://pbs.twimg.com/profile_images/429055564262277120/fjuVYyFu.jpeg","https://pbs.twimg.com/profile_images/551143684671291392/Nx_lx21L_400x400.jpeg","http://chan.catiewayne.com/b/src/137230208038.jpg"];
 
 function setup() {
   background(255,255,255);
@@ -40,28 +41,25 @@ function setup() {
 
 function draw() {
 
-  img_width = img.width;
-  img_height = img.height;
-
   if(start){
     //the image will follow the image's movement, so we can use collision from p5sprite on the DOM
     img.position(s.position.x-(img_width/2), s.position.y-(img_height/2));
-    img2.position(s2.position.x-(img_width/2), s2.position.y-(img_height/2));
+    img2.position(s2.position.x-(img2_width/2), s2.position.y-(img2_height/2));
     //floored since sprites can have non integer positions, while DOM image objects can't
     y_velocity += Math.floor(acceleration);
+    y2_velocity += Math.floor(acceleration);
     s.position.y = s.position.y + y_velocity;
-    s2.position.y = s2.position.y + y_velocity;
+    s2.position.y = s2.position.y + y2_velocity;
     //check collision with line
     s.bounce(line, invert);
-    s2.bounce(line, invert);
+    s2.bounce(line, invert2);
   }
   drawSprites();
 }
 
 function invert(){
     y_velocity = Math.floor(y_velocity * -0.5);
-    s.position.y = line_height-129;
-    s2.position.y = line_height-129;
+    s.position.y = line_height-(img_height/2)+1;
     if(y_velocity == -1 && stopped == false){
         stopped = true;
         textSize(32);
@@ -69,6 +67,12 @@ function invert(){
         fill(0, 0, 0);
         text("This or that?", (width/2), 450);
     }
+}
+
+function invert2(){
+    y2_velocity = Math.floor(y2_velocity * -0.5);
+    s2.position.y = line_height-(img2_height/2)+1;
+
 }
 
 function checkDimensions(){
@@ -86,11 +90,11 @@ function spawn(){
     //makes our DOM image object un-highlight-able, so it's indistinguishable from the canvas
     img.class("noselect");
     s = createSprite(width/3, 10, img_width, img_height);
-    s2 = createSprite(2*(width/3), 10, img_width, img_height);
+    s2 = createSprite(2*(width/3), 10, img2_width, img2_height);
     s.shapeColor = color(0, 0, 0, 0);
     s2.shapeColor = color(0, 0, 0, 0);
     img.position(s.position.x-(img_width/2), 10-(img_height/2));
-    img2.position(s.position.x-(img_width/2), 10-(img_height/2));
+    img2.position(s.position.x-(img2_width/2), 10-(img2_height/2));
 
 }
 
@@ -98,6 +102,7 @@ function mousePressed() {
     background(255,255,255);
     stopped = false;
     start = true;
+    y2_velocity = 0;
     y_velocity = 0;
     acceleration = 1;
     img.remove();
