8cf45242c3b42d984eb375b3c77f33f8b030af50
slsp8752 <slsp8752@colorado.edu>
2015-11-05 08:34:31 -0700

    Fixed question ordering issue, tweaked indexing a bit

--- sketch.js
+++ sketch.js
@@ -11,6 +11,10 @@
 Sprites are positioned from their center, while DOM images are positioned from their top left corner.
 To deal with this, we subtract half the image width/height from the values of the sprite to position the DOM image.
 */
+var questions = ["Pizza or Hot Dogs?", "Would you rather eat a scorpion or a nail?", "47 or 62?", "Mice or Rice?"];
+var left = ["Pizza", "Scorpion", "47", "Mice"];
+var right = ["Hot Dogs", "Nail", "62", "Rice"];
+var img_bool = [true, false, false, false];
 
 var y_velocity = 0;
 var y2_velocity = 0;
@@ -25,7 +29,8 @@ var img2;
 var start = false;
 var stopped = false;
 var answers = [];
-var urls = ["/eating-grass.jpg", "http://www.myfunnyreaction.com/media/k2/items/src/f53115cacd70cd7c73e329ab0a729712.jpg","http://petcaretips.net/turtle_waving.gif","http://wristbandbros.s3.amazonaws.com/assets/blog/0431.png"];
+var to_ask_index = 0;
+var urls = ["http://previewcf.turbosquid.com/Preview/2014/08/01__22_25_34/256x256.jpgeda9e67c-b397-47b3-a12b-2fd410090bfbLarge.jpg", "http://www.fancyicons.com/free-icons/220/foods/png/256/hot_dog2_256.png","http://petcaretips.net/turtle_waving.gif","http://wristbandbros.s3.amazonaws.com/assets/blog/0431.png"];
 function setup() {
     background(255,255,255);
     var canv = createCanvas(windowWidth,windowHeight);
@@ -52,19 +57,25 @@ function draw() {
     y2_velocity += Math.floor(acceleration);
     s.position.y = s.position.y + y_velocity;
     s2.position.y = s2.position.y + y2_velocity;
-    //check collision with line
+
+    if (s.position.y > line_height){
+        s.position.y = line_height;
+    }
+    if (s2.position.y > line_height){
+        s2.position.y = line_height;
+    }
     s.bounce(line, invert);
     s2.bounce(line, invert2);
 
     if(stopped && s.mouseIsPressed){
         newImages();
-        answers.push("A");
-        console.log(answers);
+        //answers.push("A");
+        //console.log(answers);
     }
     if(stopped && s2.mouseIsPressed){
         newImages();
-        answers.push("B");
-        console.log(answers);
+        //answers.push("B");
+        //console.log(answers);
     }
   }
   drawSprites();
@@ -78,7 +89,10 @@ function invert(){
         textSize(32);
         textAlign(CENTER);
         fill(0, 0, 0);
-        text("This or that?", (width/2), 450);
+        if(to_ask.length > 0){
+            text(to_ask[0], (width/2), 450);
+        }
+        
     }
 }
 
@@ -93,10 +107,8 @@ function spawn(){
     img2.removeClass("hide");
     img.class("noselect");
     img2.class("noselect");
-    console.log()
     s = createSprite(width/3, 10, img_width, img_height);
     s2 = createSprite(2*(width/3), 10, img2_width, img2_height);
-    console.log('Dimensions!: ' + img_width + ", " + img_height + ", " + img2_width + ", " + img2_height)
     s.mouseActive = true;
     s2.mouseActive = true;
     s.shapeColor = color(0, 0, 0, 0);
@@ -108,14 +120,13 @@ function spawn(){
 
 function mousePressed(){
     if(start == false){
-        console.log("Start!")
+        console.log("Start!");
         newImages();
     }
 }
 
 function doStuff() {
     if(img_width == 0 || img_height == 0 || img2_width == 0 || img2_height == 0) {
-        console.log("Dimensions: " + img_width + ", " + img_height + ", " + img2_width + ", " + img2_height);
         img_width = img.width;
         img_height = img.height;
         img2_width = img2.width;
@@ -136,10 +147,39 @@ function newImages() {
     img2.remove();
     s.remove();
     s2.remove();
-    image1url = urls[Math.floor(Math.random() * urls.length)];
-    image2url = urls[Math.floor(Math.random() * urls.length)];
-    img = createImg(image1url);
-    img2 = createImg(image2url);
+    //Randomly grab question from list of unasked questions
+    to_ask_index = Math.floor(Math.random() * questions.length);
+    //Remove that question from the pool of questions to be asked
+    to_ask = questions.splice(to_ask_index, 1);
+    //if question needs images, load image. otherwise, load text.
+    if(img_bool[to_ask_index] == true){
+
+        image1url = urls[0];
+        image2url = urls[1];
+        img = createImg(image1url);
+        img2 = createImg(image2url);
+
+        img_bool.splice(to_ask_index,1);
+        left.splice(to_ask_index, 1);
+        right.splice(to_ask_index,1);
+
+    }
+    else if(img_bool[to_ask_index] == false){
+        
+
+        img_bool.splice(to_ask_index,1);
+        left_text = left.splice(to_ask_index, 1);
+        right_text = right.splice(to_ask_index,1);
+        img = createElement('h2',left_text);
+        img2 = createElement('h2',right_text);
+
+    }
+    else{
+        textSize(32);
+        textAlign(CENTER);
+        text("No more questions!", (width/2), 450);
+    }
+
     img.class("hide");
     img2.class("hide");
     img_width = 0;
