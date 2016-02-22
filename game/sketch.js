038f5d9963ea38b59091b55ebf451730c2a9fd38
slsp8752 <slsp8752@colorado.edu>
2016-02-21 18:49:37 -0700

    Removed sprite baased animation

--- game/sketch.js
+++ game/sketch.js
@@ -13,20 +13,14 @@ var question_text;
 var questions = [];
 var left = [];
 var right = [];
-var img_bool = [];
 var q_nums = [];
 for(var i = 0; i < q_json.length; i++){
 
     questions.push(q_json[i].question_text);
     left.push(q_json[i].question_left);
     right.push(q_json[i].question_right);
-    img_bool.push(q_json[i].image);
     q_nums.push(q_json[i].question_number);
 }
-//var questions = ["Pizza or Hot Dogs?", "Would you rather eat a scorpion or a nail?", "47 or 62?", "Mice or Rice?", "A or B?", "C or D?", "2 or K?", "Yes or No?", "Now or later?", "Uh or Huh?", "Cars or Cats?", "W or U?"];
-//var left = ["Pizza", "Scorpion", "47", "Mice", "A", "C", "2", "Yes", "Now", "Uh", "Cars", "W"];
-//var right = ["Hot Dogs", "Nail", "62", "Rice", "B", "D", "K", "No", "Later", "Huh", "Cats", "U"];
-// var mock_answers = ['1B','2B','3B','4B','5B','6B','7B','8B','9B','10B','11B','12B','13B','14B','15B','16B','17B','18B','19B','20B','21B','22B','23B','24B','25B','26B','27B','28B','29B','30B'];
 
 var mock_answers = {
     1: "B",
@@ -62,7 +56,6 @@ var mock_answers = {
     31: "B",
     32: "B"
 };
-//var img_bool = [true, false, false, false, false, false, false, false, false, false, false, false];
 var percent = 0;
 var results_timer;
 var timerinterval;
@@ -70,12 +63,9 @@ var prog_done = false;
 var y_velocity = 0;
 var y2_velocity = 0;
 var acceleration = 10;
-var img_width = 0;
-var img_height = 0;
-var img2_width = 0;
-var img2_height = 0;
-var img;
-var img2;
+var left_choice;
+var right_choice;
+var stopped = false;
 var start = false;
 var end = false;
 var wrong_prompt = false;
@@ -105,16 +95,11 @@ function setup() {
     myTimer.hide();
     question_text = createDiv('Click To Start'); //dummy, so that we can have remove() as the first line of the newquestion function
     question_text.class("questionText");
-    question_text
     var canv = createCanvas(windowWidth,windowHeight);
     canv.parent("bgCanvas");
     background("#8bc4b6");
-    s = createSprite(0,0,0,0);
-    s2 = createSprite(0,0,0,0);
-    s.mouseActive = true;
-    s2.mouseActive = true;
-    img = createImg("blank.png");
-    img2 = createImg("blank.png");
+    left_choice = createImg("blank.png");
+    right_choice = createImg("blank.png");
     aspect = podium.width/podium.height;
     pod_width = width * 0.75;
     pod_height = podium.height/(podium.width/(pod_width))
@@ -130,155 +115,110 @@ var Round1 = function () {
     this.QR1 = [];
     this.q_num = 0;
     this.ready = false;
-    this.stopped = false;
     this.question;
 
     this.createQuestions = function () {
         for (i=0; i<10; i++) {
             var num = _.random(0, q_json.length - 1);
             var myQues = q_json[num]
-            console.log(myQues);
+            //console.log(myQues);
             this.QR1.push(myQues);
             _.remove(q_json, function (n) {
                 return n["question_number"] == myQues['question_number'];
             });
         }
 
-    },
+    }
 
     this.start= function () {
         this.createQuestions();
         this.drawCanvas();
         this.questionTime();
-    },
+    }
 
     this.questionTime= function () {
+        //console.log(this);
         if (this.q_num < 10) {
             this.askQuestion()
         } else {
+            //console.log("About to transition");
             transitionGame(2);
         }
     }
 
     this.askQuestion= function () {
-        console.log('asking a question :)')
+        //console.log('asking a question :)')
         timerinterval = setInterval(update_timer, 1000);
         question_text.remove();
-        this.stopped = false;
+        stopped = false;
         y2_velocity = 0;
         y_velocity = 0;
         acceleration = 1;
-        img.remove();
-        img2.remove();
-        s.remove();
-        s2.remove();
+        left_choice.remove();
+        right_choice.remove();
         this.question = this.QR1[this.q_num];
         //Increment for next pass
-        this.q_num++;
+        this.q_num += 1;
         left_text = this.question['question_left'];
         right_text = this.question['question_right'];
         question_text = this.question['question_text'];
-        img = createElement('h2',left_text);
-        img2 = createElement('h2',right_text);
-
-        s = createSprite(width/3, 10, img_width, img_height);
-        s2 = createSprite(2*(width/3), 10, img2_width, img2_height);
-        s.shapeColor = color(0, 0, 0, 0);
-        s2.shapeColor = color(0, 0, 0, 0);
-        img.position(s.position.x-(img_width/2), 10-(img_height/2));
-        img2.position(s.position.x-(img2_width/2), 10-(img2_height/2));
-        this.update();
-    },
-
-    this.update= function () {
-        while (this.stopped == false) {
-            console.log(this.stopped);
-            this.updateSprite();
-            console.log('updating sprites');
-        }
-        console.log('ready for question');
+        left_choice = createElement('h2',left_text);
+        right_choice = createElement('h2',right_text);
+        console.log("Make sprites?");
+        left_choice.position(100, 200);
+        right_choice.position(300, 200);
         question_text = createDiv(this.question['question_text']);
         question_text.class("questionText");
         question_text.class("noselect");  
-    },
-
-    this.updateSprite= function () {
-        img.position(s.position.x-(img_width/2), s.position.y-(img_height/2));
-        img2.position(s2.position.x-(img2_width/2), s2.position.y-(img2_height/2));
-        //floored since sprites can have non integer positions, while DOM image objects can't
-        y_velocity += Math.floor(acceleration);
-        y2_velocity += Math.floor(acceleration);
-        s.position.y = s.position.y + y_velocity;
-        s2.position.y = s2.position.y + y2_velocity;
-
-        if (s.position.y > line_height){
-            s.position.y = line_height;
-        }
-        if (s2.position.y > line_height){
-            s2.position.y = line_height;
-        }
-        s.bounce(line, this.invert);
-        drawSprites();
-    },
-
-    this.invert= function () {
-        console.log('invert so hard');
-        console.log(this.stopped);
-        y_velocity = Math.floor(y_velocity * -0.5);
-        y2_velocity = Math.floor(y2_velocity * -0.5);
-        s2.position.y = line_height-5-(img_height/2);
-        s.position.y = line_height-5-(img_height/2);
-        if(y_velocity <= -1 && this.stopped == false){
-            console.log(y_velocity)
-            this.stopped = true;
-            fill(0, 0, 0);
-        }
-    },
-
+    }
 
     this.leftFire= function () {
-        myq = parseInt(question["question_number"]);
+        me = this;
+        myq = parseInt(this.question["question_number"]);
         mya = mock_answers[myq];
         answers_ten.push({myq: 'A'});
         if ('A' == mya) {
-            img.attribute('background-color', 'green');
-            setTimeout(this.questionTime, 1000);
+            left_choice.attribute('background-color', 'green');
+            setTimeout(function () {me.questionTime()}, 1000);
         } else {
             wrong_count++;
-            wrongDisplay();
+            this.wrongDisplay();
         }
 
-    },
+    }
 
     this.rightFire = function () {
-        myq = parseInt(question["question_number"]);
+        me = this;
+        myq = parseInt(this.question["question_number"]);
         mya = mock_answers[myq];
         answers_ten.push({myq: 'B'});
         if ('B' == mya) {
-            img2.attribute('background-color', 'green');
-            setTimeout(this.questionTime, 1000);
+            right_choice.attribute('background-color', 'green');
+            setTimeout(function () {me.questionTime()}, 1000);
         } else {
             wrong_count++;
-            wrongDisplay();
+            this.wrongDisplay();
         }
-    },
+    }
 
     this.wrongDisplay = function () {
+        me = this;
         imageMode(CORNER);
         image(overlay, 0, 0, width, height);
         text("DesignCraft would like you to take a moment to think about what you just said.", width/2, 300);
-        img.class("hide");
-        img2.class("hide");
+        left_choice.class("hide");
+        right_choice.class("hide");
         question_text.class("hide");
-        setTimeout(this.endWrong, 5000);
-    },
+        setTimeout(function () {me.endWrong()}, 5000);
+    }
 
     this.endWrong = function () {
-        img.removeClass("hide"); 
-        img2.removeClass("hide"); 
+        left_choice.removeClass("hide"); 
+        right_choice.removeClass("hide"); 
         question_text.removeClass("hide");
+        this.drawCanvas();
         this.questionTime();
-    },
+    }
 
     this.drawCanvas = function () {
         clear();
@@ -288,6 +228,7 @@ var Round1 = function () {
         background("#8bc4b6");
         imageMode(CENTER);
         rectMode(CENTER);
+        fill(255);
         rect(width/2, line_height-200, width*0.75, line_height);
         image(podium, width/2, height - (pod_height/2), pod_width, pod_height);
         textSize(32);
@@ -316,19 +257,19 @@ var Transition = function ( state ) {
             mock_answers = myRecs;
             text("Press Enter when you're ready to keep it going!", width/2, 500);
             done = true;
-        },
+        }
         error: function(xhr) {
             alert("An error occured: " + xhr.status + " " + xhr.statusText);
         }
     });
         */
-    },
+    }
 
     this.finisher = function () {
         if (done == true) {
             //Recognize the key press
         }
-    },
+    }
 
     this.display = function () {
         if (myState == 2){
@@ -445,4 +386,5 @@ function increase_percentage(){
         prog_done = true;
     }
 }
+
 
